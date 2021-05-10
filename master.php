2e742ce803ca571246d58935799f2c6ed9188d7a
pankoza-pl <77279607+pankoza-pl@users.noreply.github.com>
2021-05-10 08:53:21 +0200

    Update master.php

--- master.php
+++ master.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ProgramistaZpolski\kasztanLang;
+namespace pankoza\cementLang;
 
 if (!function_exists('str_contains')) {
     function str_contains(string $haystack, string $needle): bool {
@@ -17,14 +17,14 @@ class Compiler
 		foreach ($data as $key => $value) {
 			$v = str_replace("	", "", $value);
 			$v = str_replace("\n", "", $v);
-			if ($v == "kasztan+") {
+			if ($v == "cement+") {
 				$count++;
-			} else if ($v == "kasztan-") {
+			} else if ($v == "cement-") {
 				$count--;
-			} else if ($v == "kasztan") {
+			} else if ($v == "cement") {
 				echo $count . " ";
-			} else if (preg_match("/kasztan_([a-z]+).php/", $v)) {
-				preg_match_all("/kasztan_([a-z]+).php/", $v, $matches);
+			} else if (preg_match("/cement_([a-z]+).php/", $v)) {
+				preg_match_all("/cement_([a-z]+).php/", $v, $matches);
 				require_once $matches[1][0] . ".php";
 			}
 		}
